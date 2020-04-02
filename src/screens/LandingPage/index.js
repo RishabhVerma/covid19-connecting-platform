213a6337bcd2b999d5c55b2608730fcd064c481a
Rishabh Verma <me@rishabhverma.me>
2020-04-02 12:36:02 +0530

    Changes.

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
 
 export class LandingPage extends React.Component {
   render() {
     return (
-      <Typography variant="h2">Landing Page under Construction</Typography>
+      <>
+        <Link to="/peopleInNeed">Hello</Link>
+        <Typography variant="h2">Landing Page under Construction</Typography>
+      </>
     );
   }
 };
