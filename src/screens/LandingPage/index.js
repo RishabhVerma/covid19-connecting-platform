add73b3957432870f4f208956dbb3985b25ddea5
Rishabh Verma <me@rishabhverma.me>
2020-04-02 13:04:59 +0530

    Minor fix.

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 
 
 export class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <Link to="/peopleInNeed">Hello</Link>
         <Typography variant="h2">Landing Page under Construction</Typography>
       </>
     );
