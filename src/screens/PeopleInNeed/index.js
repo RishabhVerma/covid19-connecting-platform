d584f109d5a944d94266f533468538107272f5cc
Rishabh Verma <me@rishabhverma.me>
2020-04-05 14:36:07 +0530

    changes in people in need.

--- src/screens/PeopleInNeed/index.js
+++ src/screens/PeopleInNeed/index.js
@@ -171,16 +171,20 @@ class PeopleInNeed extends React.Component {
           <Spacer height={theme.spacing(2)} />
           <Typography variant="h3">HELP US, HELP THEM</Typography>
           <Spacer height={theme.spacing(1)} />
-          <Typography variant="body1">{'We have a team of volunteers and social activists working relentlessly in and around Delhi and UP to deliver essentials to stranded(due to lockdown) migrant workers or residents of densely packed slums or another word for underserved. We have helped about 120 families till now and over thousands await. All help in terms of money and labour is welcome and needed desperately.'}</Typography>
-          <Spacer height={theme.spacing(1)} />
-          <Typography variant="body1">{'Here is how you can help.'}</Typography>
+          <Typography variant="body1" align="justify">{'We have a team of volunteers and social activists working relentlessly in and around Delhi and UP to deliver essentials to stranded(due to lockdown) migrant workers or residents of densely packed slums or another word for underserved. We have helped about 120 families till now and over thousands await. All help in terms of money and labour is welcome and needed desperately.'}</Typography>
+          <br />
+          <Typography variant="body1" align="justify" style={{fontWeight: 900}}>
+            {'We are working on building this web page which will show you a list of all people who need help in the areas we are working in. Currently you can see a list of few such people who need help. We are working on verifying the data and showing you a complete list in a few days.'}
+          </Typography>
+          <br />
           <Typography variant="body1">{'For volunteering on-ground, please call 899765210.'}</Typography>
-          <Typography variant="body1">{'To donate funds directly to beneficiaries, please browse through this crowd-sourced and further verified by our team.'}</Typography>
+          <br />
+          {/* <Typography variant="body1">{'To donate funds directly to beneficiaries, please browse through this crowd-sourced and further verified by our team.'}</Typography>
           <Typography variant="body1">{'You can also just donate money to us. We are in the process of setting up an on online page to collect payments. Till then you can get in touch with us directly on the number given above.'}</Typography>
           <Spacer height={theme.spacing(1)} />
           <Typography variant="body1">{'Here is a list of all the people who need help. You can filter the list by state using the dropdown below.'}</Typography>
-          <Spacer height={theme.spacing(2)} />
-          <FormControl variant="outlined" className={classes.formControl}>
+          <Spacer height={theme.spacing(2)} /> */}
+          {/* <FormControl variant="outlined" className={classes.formControl}>
             <InputLabel id="state-filter-label">State</InputLabel>
             <Select
               labelId="state-filter-label"
@@ -195,7 +199,7 @@ class PeopleInNeed extends React.Component {
               <MenuItem value={'Uttar Pradesh'}>Uttar Pradesh</MenuItem>
             </Select>
           </FormControl>
-          <Spacer height={theme.spacing(2)} />
+          <Spacer height={theme.spacing(2)} /> */}
           { beneficariesLoading ? <Typography variant="body1">Loading...</Typography> : this.renderAllCards() }
           <Spacer height={theme.spacing(2)} />
 
